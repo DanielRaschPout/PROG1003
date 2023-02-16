e00bd1ff518047030944203a1be220f6a61d840c
Daniel Fernando Petter Rasch-Pout <dfraschp@gmail.com>
2023-02-16 03:29:40 +0100

    doxygen

--- heftet/oppgave8.cpp
+++ heftet/oppgave8.cpp
@@ -12,15 +12,7 @@
  * @copyright Copyright (c) 2023
  * 
  */
- * @file oppgave8.cpp
- * @author Daniel Fernando Petter Rasch-Pout (dfraschp@gmail.com)
- * @brief 
- * @version 0.1
- * @date 16-02-2023
- * 
- * @copyright Copyright (c) 2023
- * 
- */
+
 #include <iostream>                //  cout, cin
 #include <string>                  //  string-klassen
 #include <vector>                  //  vector
